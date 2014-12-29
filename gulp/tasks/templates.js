8fed925b4d07e5100bdaf11a6a9c1548f4e4cf70
Blaise Laflamme <blaise@laflamme.org>
2014-12-29 16:19:02 -0500

    added templates task

---
+++ gulp/tasks/templates.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var swig = require('gulp-swig');
+var data = require('gulp-data');
+var prettify = require('gulp-prettify');
+var config = require('../config').templates;
+
+
+var getJsonData = function(file) {
+  var jsonFile = file.path.replace('.html', '.json');
+  try {
+    return require(jsonFile);
+  } catch (err) {
+    console.log(err);
+    return
+  }
+};
+
+
+gulp.task('templates', function () {
+  console.log('build templates');
+  return gulp.src(config.src, {base: config.path})
+    .pipe(data(getJsonData))
+    .pipe(swig())
+    .pipe(prettify({indent_size: 2}))
+    .pipe(gulp.dest(config.dest));
+});
