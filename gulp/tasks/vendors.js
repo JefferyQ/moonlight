fe7c513082b9419c6b3b8ac4249ed4494ff14df1
Blaise Laflamme <blaise@laflamme.org>
2015-01-02 20:15:47 -0500

    remove unused vendor src and sourcemaps

--- gulp/tasks/vendors.js
+++ gulp/tasks/vendors.js
@@ -11,26 +11,14 @@ var
   ;
 
 
-var src = [];
-for(var key in PACKAGE.browser) {
-  if(!buildMode.dist || dist.pruneVendors.indexOf(key) < 0) {
-    src.push(PACKAGE.browser[key]);
-  }
-}
-
-
 gulp.task('vendors', function() {
   gulp.src(config.css.src)
-    .pipe(sourcemaps.init())
     .pipe(concat(config.css.outputNameMinified))
     .pipe(minify())
-    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(config.css.dest))
   gulp.src(config.js.src)
-    .pipe(sourcemaps.init())
     .pipe(concat(config.js.outputNameMinified))
     .pipe(uglify())
-    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(config.js.dest))
   gulp.src(config.fonts.src)
     .pipe(gulp.dest(config.fonts.dest))
