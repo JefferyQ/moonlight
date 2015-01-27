d35bb03b0eddca8119b381460bbeaacbfb21be08
Blaise Laflamme <blaise@laflamme.org>
2015-01-27 08:44:10 -0500

    fix vendors buildMode

--- gulp/tasks/vendors.js
+++ gulp/tasks/vendors.js
@@ -1,25 +1,54 @@
 var
+  browserify = require('browserify');
   gulp = require('gulp'),
   concat = require('gulp-concat'),
-  sourcemaps = require('gulp-sourcemaps'),
   uglify = require('gulp-uglify'),
   minify = require('gulp-minify-css'),
+  streamify = require('gulp-streamify'),
+  util = require('gulp-util'),
+  source = require('vinyl-source-stream'),
+  bundleLogger = require('../util/bundleLogger'),
+  handleErrors = require('../util/handleErrors'),
   config = require('../config').vendors,
-  buildMode = require('../config').buildMode,
-  dist = require('../config').dist,
-  PACKAGE = require('../../package.json')
+  buildMode = require('../config').buildMode
   ;
 
 
-gulp.task('vendors', function() {
+gulp.task('vendors', function () {
+  // CSS
+  var cssName = buildMode.dist === false ?
+    config.css.outputName : config.css.outputNameMinified;
   gulp.src(config.css.src)
-    .pipe(concat(config.css.outputNameMinified))
-    .pipe(minify())
+    .pipe(concat(cssName))
+    .pipe(buildMode.dist === true ?
+      minify() : util.noop()
+    )
     .pipe(gulp.dest(config.css.dest))
-  gulp.src(config.js.src)
-    .pipe(concat(config.js.outputNameMinified))
-    .pipe(uglify())
-    .pipe(gulp.dest(config.js.dest))
+  // MAPS
+  if (buildMode.dist === false) {
+    gulp.src(config.css.map)
+      .pipe(gulp.dest(config.css.dest))
+  }
+  // FONTS
   gulp.src(config.fonts.src)
     .pipe(gulp.dest(config.fonts.dest))
+  // JS
+  var jsName = buildMode.dist === false ?
+    config.js.outputName : config.js.outputNameMinified;
+  var bundler = browserify({debug: false});
+  config.js.lib.forEach(function (lib) {
+    bundler.require(lib);
+  });
+  bundleLogger.start(jsName);
+  bundler
+    .bundle()
+    .on('error', handleErrors)
+    .pipe(source(jsName))
+    .pipe(buildMode.dist === true ?
+      streamify(uglify()) : util.noop()
+    )
+    .pipe(gulp.dest(config.js.dest))
+    .on('end', function() {
+      bundleLogger.end(jsName)
+    });
 });
