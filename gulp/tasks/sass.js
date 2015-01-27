82c132f950303bf1996f1b4ab795f985ac08cf8e
Blaise Laflamme <blaise@laflamme.org>
2015-01-27 08:44:19 -0500

    fix sass buildMode

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -4,19 +4,25 @@ var
   concat = require('gulp-concat'),
   sourcemaps = require('gulp-sourcemaps'),
   minify = require('gulp-minify-css'),
+  util = require('gulp-util'),
   handleErrors = require('../util/handleErrors'),
-  config = require('../config').sass
+  config = require('../config').sass,
+  buildMode = require('../config').buildMode
   ;
 
 
 gulp.task('sass', function() {
+  var cssName = buildMode.dist === false ?
+    config.outputName : config.outputNameMinified;
   gulp.src(config.src)
-    .pipe(sourcemaps.init())
+    .pipe(buildMode.dist === false ? sourcemaps.init() : util.noop())
     .pipe(sass()
       .on('error', handleErrors)
     )
-    .pipe(concat(config.outputNameMinified))
-    .pipe(minify())
-    .pipe(sourcemaps.write('.'))
+    .pipe(concat(cssName))
+    .pipe(buildMode.dist === true ? minify() : util.noop())
+    .pipe(buildMode.dist === false ?
+      sourcemaps.write('.') : util.noop()
+    )
     .pipe(gulp.dest(config.dest));
 });
