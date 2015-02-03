c6137a14f8c949abc03359963202f8371c439556
Blaise Laflamme <blaise@laflamme.org>
2015-02-02 19:40:00 -0500

    require and use pace

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,12 @@
 // Imports
 $ = jQuery = require('jquery');
 var bootstrap = require('bootstrap');
+var Pace = require('pace');
 var sidebar = require('./sidebar');
 var header = require('./header');
 
+//
+Pace.start();
 //
 sidebar();
 header();
